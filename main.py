27bcf9a7c6579f39c9836aad80eefc800975de7f
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-04-24 01:46:44 +0300

    feature: add the login validation and user creation function

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("Main is working")
+from user import User
+
+user_accounts = []
+
+print("Welcome to the password vault!")
+
+while True:
+    print("\nWhat would you like to do? ")
+    choice = input("a: open an account\nb: login to your existing account\n\n"
+                   "Type 'a' for the first option and 'b' for the second: ")
+
+    if choice.lower() == "a":
+        first_name = input("Enter your first name: ")
+        last_name = input("Enter your last name: ")
+        user_name = input("Enter your desired username: ")
+        user_password = input("Enter your desired password: ")
+
+        user_name = User(first_name, last_name, user_name, user_password)
+        print("\n - - - Account successfully created! - - - ")
+
+        user_accounts.append(user_name)
+
+    else:
+        login_name = input("Enter your username: ")
+        login_password = input("Enter your password: ")
+        for user in user_accounts:
+            if login_name == user.username and login_password == user.password:
+                print("\n - - - Welcome to your account - - - ")
+            else:
+                print("\n - - - Your details are invalid - - - ")
+
+
+
